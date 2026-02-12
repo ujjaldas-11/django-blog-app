bfbadc46a4a674b5054786916f9b316505e1142e
ujjaldas-11 <dasu82058@gmail.com>
2026-02-12 18:37:57 +0530

    created models & views

--- tweet/urls.py
+++ tweet/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.tweet_list, name="tweet_list"),
+    path('create/', views.create_tweet, name="create_tweet"),
+    path('edit/<int: tweet_id>/', views.create_tweet, name="create_tweet"),
+    path('delete/<int: tweet_id>/', views.create_tweet, name="create_tweet"),
 ]
--- tweet/views.py
+++ tweet/views.py
@@ -1,4 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Tweet
+from .forms import TweetForm
 
 def index(request):
     return render(request, 'index.html')
+    
+
+
+def tweet_view(request):
+    tweets = Tweet.objects.all()
+    return render(request, 'index.html', {'tweets': tweets})
+
+def create_tweet(request):
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            form.save()
+        return redirect('tweet_view')
+    else:
+        form = TweetForm()
+    return render(request, 'tweet_form.html', {'form': form})
+
+def update_tweet(request):
+    tweet= get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES, instance=tweet)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            form.save()
+            return redirect('tweet_view')
+    else:
+        form = TweetForm(instance=tweet)  
+    return render(request, 'tweet_form.html', {'form': form})
+
+
+def tweet_delete(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+
+    if request.method == 'POST':
+        tweet.delete()
+        return redirect('tweet_view')
+    return render(request, 'tweet_confirm_delete.html', {'tweet': tweet})
