687fce49f1d2616c9c31976fa53d660f8a8888c6
ujjaldas-11 <dasu82058@gmail.com>
2026-02-14 17:19:05 +0530

    create delete tweet feature

--- tweet/urls.py
+++ tweet/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.tweet_list, name="tweet_list"),
     path('create/', views.create_tweet, name="create_tweet"),
-    path('edit/<int:pk>/', views.update_tweet, name="edit-tweet"),
-    path('delete/<int:pk>/', views.delete_tweet, name="delete_tweet"),
+    path('edit/<int:tweet_id>/', views.update_tweet, name="edit-tweet"),
+    path('delete/<int:tweet_id>/', views.delete_tweet, name="delete-tweet"),
 ]
