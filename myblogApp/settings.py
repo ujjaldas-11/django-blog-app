6af6183cded4a64e1d139ec8706d42f8ef25e241
ujjaldas-11 <dasu82058@gmail.com>
2026-03-04 17:06:24 +0530

    modified settings.py file

--- myblogApp/settings.py
+++ myblogApp/settings.py
@@ -194,7 +194,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
     
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] if os.path.exists(STATIC_DIR) else []
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] if os.path.exists(STATIC_DIR) else []
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
 MEDIA_URL="/media/"
